e55828f0e3b3504914bfeee57c535a21c9989a8c
karamon17 <gevorik18@gmail.com>
2023-08-23 09:51:05 +0400

    ex03 is ready, so I am ready to eval

--- Module_02/ex03/bsp.cpp
+++ Module_02/ex03/bsp.cpp
@@ -2,9 +2,7 @@
 
 Fixed   abs(Fixed x)
 {
-    if (x < 0)
-        return x * -1;
-    return x;
+	return (x < 0) ? (x * -1) : x;
 }
 
 Fixed   Square( Point const a, Point const b, Point const c ) 
@@ -17,8 +15,8 @@ Fixed   Square( Point const a, Point const b, Point const c )
 bool bsp(Point const a, Point const b, Point const c, Point const point) 
 {
     Fixed   abcS = abs(Square( a, b, c ));
-    Fixed   aS = abs(Square( point, a, b ));
-    Fixed   bS = abs(Square( point, b, c ));
-    Fixed   cS = abs(Square( point, c, a ));
-    return  ((abcS == aS + bS + cS) && aS != 0 && bS != 0 && cS != 0);
+    Fixed   abS = abs(Square( point, a, b ));
+    Fixed   bcS = abs(Square( point, b, c ));
+    Fixed   caS = abs(Square( point, c, a ));
+    return  ((abcS == abS + bcS + caS) && abS != 0 && bcS != 0 && caS != 0);
 }
