8e7e5b5e86031cbd6e928785c3e25d83974ce597
karamon17 <gevorik18@gmail.com>
2023-08-28 19:00:27 +0400

    ex01 is ready !

--- Module_04/ex01/main.cpp
+++ Module_04/ex01/main.cpp
@@ -6,6 +6,11 @@
 
 int main()
 {
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	delete j;//should not create a leak
+	delete i;
+
 	Animal *animals[10];
 	for (int i = 0; i < 5; i++)
 		animals[i] = new Dog();
