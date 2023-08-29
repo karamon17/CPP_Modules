7ed422d2ae7975a64bc7df9b9b1c2f6a53ec4a8e
karamon17 <gevorik18@gmail.com>
2023-08-29 10:08:52 +0400

    ex02 is ready

---
+++ Module_04/ex02/Animal.hpp
@@ -0,0 +1,23 @@
+#ifndef ANIMAL_HPP
+# define ANIMAL_HPP
+
+# include <iostream>
+
+class Animal
+{
+	protected:
+		std::string type;
+	
+	public:
+		Animal();
+		Animal(const Animal &copy);
+		Animal &operator=(const Animal &copy);
+		virtual ~Animal();
+
+		virtual void makeSound() const = 0;
+		std::string getType() const;
+		void setType(std::string type);
+};
+
+
+#endif
---
+++ Module_04/ex02/Dog.cpp
@@ -0,0 +1,27 @@
+#include "Dog.hpp"
+
+Dog::Dog() : Animal() {
+    std::cout << "Dog constructor called" << std::endl;
+    type = "Dog";
+	brain = new Brain();
+}
+
+Dog::~Dog() {
+    std::cout << "Dog destructor called" << std::endl;
+	delete brain;
+}
+
+Dog::Dog(const Dog &copy) : Animal(copy) {
+    std::cout << "Dog copy constructor called" << std::endl;
+}
+
+Dog &Dog::operator=(const Dog &copy) {
+    std::cout << "Dog assignation operator called" << std::endl;
+    if (this != &copy)
+        Animal::operator=(copy);
+    return *this;
+}
+
+void Dog::makeSound() const {
+	std::cout << "Woof" << std::endl;
+}
