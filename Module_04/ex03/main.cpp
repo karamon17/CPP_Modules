398d7193ce44a8681504bb87050d6ec90b3daeb3
karamon17 <gevorik18@gmail.com>
2023-08-30 12:50:13 +0400

    ready to eval

--- Module_04/ex03/main.cpp
+++ Module_04/ex03/main.cpp
@@ -7,26 +7,80 @@
 
 int main()
 {
-	IMateriaSource* src = new MateriaSource();
-	src->learnMateria(new Ice());
-	src->learnMateria(new Cure());
+	std::cout << "### ICE TESTS ###" << std::endl;
+	{
+		AMateria *ice = new Ice();
+		AMateria *clone;
+		ICharacter *villain = new Character("villain");
 
-	ICharacter* me = new Character("me");
+		clone = ice->clone();
+		std::cout << "Ice type: " << clone->getType() << std::endl;
+		std::cout << "Clone type: " << clone->getType() << std::endl;
+		std::cout << "Using type: ";
+		ice->use(*villain);
 
-	AMateria* tmp;
-	tmp = src->createMateria("ice");
-	me->equip(tmp);
-	tmp = src->createMateria("cure");
-	me->equip(tmp);
+		delete villain;
+		delete clone;
+		delete ice;
+	}
+	std::cout << "\n### CURE TESTS ###" << std::endl;
+	{
+		AMateria *cure = new Cure();
+		AMateria *clone;
+		ICharacter *hero = new Character("hero");
 
-	ICharacter* bob = new Character("bob");
+		clone = cure->clone();
+		std::cout << "Cure type: " << clone->getType() << std::endl;
+		std::cout << "Clone type: " << clone->getType() << std::endl;
+		std::cout << "Using type: ";
+		cure->use(*hero);
 
-	me->use(0, *bob);
-	me->use(1, *bob);
+		delete hero;
+		delete clone;
+		delete cure;
+	}
+	std::cout << "\n### CHARACTER TESTS ###" << std::endl;
+	{
+		IMateriaSource *src = new MateriaSource();
+		ICharacter *me = new Character("me");
 
-	delete bob;
-	delete me;
-	delete src;
+		std::cout << "Character name: " << me->getName() << std::endl;
+		std::cout << "Equiping materia..." << std::endl;
+		AMateria *tmp;
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+		std::cout << "Unequiping materia..." << std::endl;
+		me->unequip(0);
+		me->unequip(1);
 
-	return 0;
+		delete me;
+		delete src;
+	}
+	std::cout << "\n### MANDATORY TESTS ###" << std::endl;
+	{
+		IMateriaSource *src = new MateriaSource();
+		src->learnMateria(new Ice());
+		src->learnMateria(new Cure());
+
+		ICharacter *me = new Character("me");
+
+		AMateria *tmp;
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+		tmp = src->createMateria("cure");
+		me->equip(tmp);
+
+		ICharacter *bob = new Character("bob");
+
+		me->use(0, *bob);
+		me->use(1, *bob);
+
+		delete bob;
+		delete me;
+		delete src;
+	}
+	//system("leaks game");
+	return (0);
 }
