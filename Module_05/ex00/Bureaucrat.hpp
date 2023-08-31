326bafd553f0c0b15edaf0914a73bc9b8c68f386
karamon17 <gevorik18@gmail.com>
2023-08-31 18:16:45 +0400

    ex00 is ready

---
+++ Module_05/ex00/Bureaucrat.hpp
@@ -0,0 +1,35 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat
+{
+	private:
+		std::string const	_name;
+		int					_grade;
+		Bureaucrat();
+	public:
+		Bureaucrat(std::string const &name, int grade);
+		Bureaucrat(Bureaucrat const &copy);
+		~Bureaucrat();
+		Bureaucrat &operator=(Bureaucrat const &copy);
+		std::string const	&getName() const;
+		int					getGrade() const;
+		void				incrementGrade();
+		void				decrementGrade();
+		class GradeTooHighException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+		class GradeTooLowException : public std::exception
+		{
+			public:
+				virtual const char *what() const throw();
+		};
+};
+
+std::ostream &operator<<(std::ostream &out, Bureaucrat const &bureaucrat);
+
+#endif
