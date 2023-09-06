fe0d937f3fd58389eedb7045dd08bec1b166d10a
karamon17 <gevorik18@gmail.com>
2023-09-06 14:31:08 +0400

    ex01 is ready

--- Module_07/ex01/iter.hpp
+++ Module_07/ex01/iter.hpp
@@ -3,6 +3,29 @@
 
 #include <iostream>
 
-template <typename T, typename K>
+template <typename T>
+void print(T &x)
+{
+	std::cout << x << ' ';
+}
+
+template <typename T>
+void add(T &x)
+{
+	x += 1;
+}
+
+template <typename T>
+void sub(T &x)
+{
+	x -= 1;
+}
+
+template <typename T>
+void iter(T *array, int length, void (*f)(T &))
+{
+	for (int i = 0; i < length; i++)
+		f(array[i]);
+}
 
 #endif
--- Module_07/ex01/main.cpp
+++ Module_07/ex01/main.cpp
@@ -1,5 +1,28 @@
 #include "iter.hpp"
 
 int main( void ) {
-	
+	int array[] = {1, 2, 3, 4, 5};
+	iter(array, 5, print);
+	std::cout << std::endl;
+	iter(array, 5, add);
+	iter(array, 5, print);
+	std::cout << std::endl;
+	iter(array, 5, sub);
+	iter(array, 5, print);
+	std::cout << std::endl;
+
+	char array2[] = {'a', 'b', 'c', 'd'};
+	iter(array2, 4, print);
+	std::cout << std::endl;
+	iter(array2, 4, add);
+	iter(array2, 4, print);
+	std::cout << std::endl;
+	iter(array2, 4, sub);
+	iter(array2, 4, print);
+	std::cout << std::endl;
+
+	std::string array3[] = {"Hello", "World", "!"};
+	iter(array3, 3, print);
+	std::cout << std::endl;
+	return 0;
 }
